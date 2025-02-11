44f85be3b24eeda3ead66d43f249c1458aba220d
thearchhero <pro_linux_nerd@disroot.org>
2025-02-11 21:33:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.feature-box').forEach((box) => {
+    box.classList.add('animate__animated', 'animate__fadeInUp');
+});
+
+document.querySelectorAll('.btn-effect').forEach((btn) => {
+    btn.classList.add('animate__animated', 'animate__pulse');
+});
